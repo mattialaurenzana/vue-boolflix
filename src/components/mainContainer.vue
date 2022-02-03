a400f8801aa12031dbc92bc9b38207ba1fffcb47
mattia laurenzana <mattilaure@gmail.com>
2022-02-03 17:33:07 +0100

    completamento milestone 1 (vengono mostrate una serie di informazioni riguardo il film una volta che si è in hover sulla copertina)

--- src/components/mainContainer.vue
+++ src/components/mainContainer.vue
@@ -5,6 +5,20 @@
                 <div class="img-container">
                     <img :src="firstImgPart+film.poster_path" alt="">
                 </div>
+                <div class="film-data">
+                    <div>
+                        <span>Titolo:</span>{{film.title}}
+                    </div>
+                    <div>
+                        <span>Titolo originale:</span>{{film.original_title}}
+                    </div>
+                    <div>
+                        <span>Lingua:</span>{{film.original_language}}
+                    </div>
+                    <div>
+                        <span>Voto:</span>{{film.vote_average}}
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -33,6 +47,7 @@ export default {
        .row{
            gap: 15px;
            justify-content: center;
+
             .card{
             background-color: black;
             width: calc((100% / 5) - 15px);
@@ -43,10 +58,37 @@ export default {
             align-items: center;
             word-break: break-word;
 
+            &:hover{
+                cursor: pointer;
+            }
+
+            &:hover img{
+                display: none;
+            }
+
+            &:hover .film-data{
+                display: block;
+            }
+
             .img-container{
                 img{
                     width: 100%;
                     height: 100%;
+                }
+            }
+
+            .film-data{
+                display: none;
+                font-size: 11px;
+                
+
+                div{
+                    margin: 5px 0;
+                    word-break: break-word;
+                    span{
+                        font-weight: bold;
+                        font-size: 12px;
+                    }
                 }
             }
         }
